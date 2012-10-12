7812486ee0f5b9764e9612fee96799b015507c0b
KirillBakanov <kirill.bakanov@zojax.com>
2012-10-12 12:13:04 +0300

    fixed font size selecting

--- src/zojax/widget/extjseditor/resources/fontsizeplugin.js
+++ src/zojax/widget/extjseditor/resources/fontsizeplugin.js
@@ -2,49 +2,49 @@ Ext.ns('Ext.ux.form.HtmlEditor');
 
 Ext.ux.form.HtmlEditor.HR = Ext.extend(Ext.util.Observable, {
 
-    init: function(cmp) {
+    init:function (cmp) {
         this.cmp = cmp;
-        var changeFont = function(combo) {
-            font_size = combo.getValue();
-            selection = this.getWin().getSelection().getRangeAt(0);
-            if (selection==null || selection=='')
-                return false;
-            //get selection content
-            content = selection.extractContents();
-            //create span and wrap it around selection
-            fontEl = document.createElement ("span");
-            fontEl.style.fontSize=font_size+'px';
-            fontEl.appendChild(content);
-            mod_text = fontEl.outerHTML;
-            old_text=fontEl.innerHTML;
-            selection.insertNode(fontEl);
-
-        }
+        var changeFont = function (combo) {
+            var font_size = combo.getValue();
+            var selection = this.getWin().getSelection();
+            if (selection.rangeCount > 0) {
+                var range = selection.getRangeAt(0);
+                var content = range.extractContents();
+                var fontEl = this.getWin().document.createElement("span");
+                fontEl.style.fontSize = font_size + 'px';
+                fontEl.setAttribute("class", "font_size");
+                fontEl.appendChild(content);
+                range.insertNode(fontEl);
+                if (fontEl.parentNode.nodeName == "SPAN" && fontEl.parentNode.getAttribute("class") == "font_size") {
+                    fontEl.parentNode.parentNode.replaceChild(fontEl, fontEl.parentNode)
+                }
+                selection.selectAllChildren(fontEl);
+            }
+        };
         // create the combo instance
         this.combo = new Ext.form.ComboBox({
-            typeAhead: true,
-            enableKeyEvents: true,
-            triggerAction: 'all',
-            lazyRender: true,
-            mode: 'local',
-            store: [8, 10, 12, 14, 16, 18, 20, 24, 26, 28, 32, 36, 40, 48],
-            width: 50,
-            listeners: {
-                scope: cmp,
-                'select': changeFont,
-                'keypress': changeFont
+            typeAhead:true,
+            enableKeyEvents:true,
+            triggerAction:'all',
+            lazyRender:true,
+            mode:'local',
+            store:[8, 10, 12, 14, 16, 18, 20, 24, 26, 28, 32, 36, 40, 48],
+            width:50,
+            listeners:{
+                scope:cmp,
+                'select':changeFont,
+                'keypress':changeFont
 
             }
 
         });
         this.cmp.on('render', this.onRender, this);
     },
-    onRender: function() {
+    onRender:function () {
         this.cmp.getToolbar().add(this.combo);
     }
 
 });
-
 
 
 
